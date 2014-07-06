6438c96362383f0347dafd5643ace792b96b7598
Rob Mayo <rbmy91@gmail.com>
2014-07-06 19:29:35 -0400

    Square drawn on board canvas
    Using directive to manipulate the canvas element. Will be using the
    canvas in the future to draw pieces.

--- scripts/board.js
+++ scripts/board.js
@@ -1,28 +1,43 @@
 (function(){
-	var board =
-		{
-			length: 0,
-			width: 0
-		};
+	var tileWidth = Math.round($(window).width() * .10);
+	var tileHeight = Math.round($(window).height() * .15);
 
 	var app = angular.module('board', []);
 
-	app.controller('boardController', function(){
-		
-		this.getLength = function(){
-			return board.length;
-		};
+	drawTiles = function(canvas){
+		var topLeft
+	}
 
-		this.getWidth = function(){
-			return board.width;
-		};
+	app.directive('myGameBoard',  function(){
+		return{
+			restrict: "A",
+			scope: {
+				'gXtiles' : '=',
+				'gYtiles' : '='
+			},
+			link: function(scope, element){
+				//we are doing 2d drawing for this board. Get the canvas context
+				var canvas = $(element[0]);
+				var ctx = element[0].getContext('2d');
+				var height = scope.gYtiles * tileHeight;
+				var width = scope.gXtiles * tileWidth;
 
-		this.setLength = function(length){
-			board.length = length;
-		};
+				//set the canvas size based on the number of tiles
+				canvas.attr("height", height);
+				canvas.attr("width", width);
 
-		this.setWidth = function(height){
-			board.height = height;
+				console.log("canvas height: " + canvas.attr('height'));
+				console.log("canvas width: " + canvas.attr('width'));
+
+				canvas.css({
+					border: '2px solid black',
+					float: 'left',
+					margin: '10pct 10 10 10'
+				});
+
+				//draw the tiles
+
+			}
 		}
 	});
 })();
